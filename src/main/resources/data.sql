8d8c15dc77ff4ad0cb138d6d8f0fe9c66fc7b4bd
Cassio Ramon Barbosa <cassio.rpb@gmail.com>
2020-11-04 00:26:57 -0300

    Adding some test cases

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,7 @@ INSERT INTO Character (id, name, description, modified) VALUES
 	(1, 'Hulk', 'Green muscle guy that smashes everything', CURRENT_TIMESTAMP()),
 	(2, 'IronMan', 'Genious Playboy Filantropist Billionarie', CURRENT_TIMESTAMP()),
 	(3, 'Thanos', 'The Crazy Titan', CURRENT_TIMESTAMP());
+	(4, 'Spider-Man', 'Your friendly neighborhood', CURRENT_TIMESTAMP());
 
 INSERT INTO Comic (id, title, description) VALUES
 	(1, 'Avengers: Endgame (2019)', 'Avengers back in time to undo the huge mess Thanos did'),
--- src/test/java/com/marvel/api/MrvappApplicationTests.java
+++ src/test/java/com/marvel/api/MrvappApplicationTests.java
@@ -1,13 +1,73 @@
 package com.marvel.api;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+import java.util.Random;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.marvel.api.model.Character;
+import com.marvel.api.repository.CharacterRepository;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class MrvappApplicationTests {
+	
+	@Autowired
+	private MockMvc mock;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	@Autowired
+	private CharacterRepository characterRepository;
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	void findAllCharacters() throws Exception {			
+		List<Character> characters = characterRepository.findAll();
+		Assertions.assertNotNull(characters);
+		Assertions.assertNotEquals(characters.size(), 0);		
+	}
+	
+	@Test
+	void characterFound() throws Exception {		
+		ResultActions result =  mock.perform(get("/characters/1"));		
+		result.andExpect(status().isOk());		
+	}
+	
+	@Test
+	void characterNotFound() throws Exception {		
+		ResultActions result =  mock.perform(get("/characters/999"));		
+		result.andExpect(status().isNotFound());		
+	}
+	
+	@Test
+	void characterWithNoComics() throws Exception {
+		
+		List<Character> characters = characterRepository.findAll();
+		
+		Character randomCharacter = choiceRandomCharacter(characters);
+		
+		ResultActions result =  mock.perform(get("/characters/" + randomCharacter.getId() + "/comics"));		
+		result.andExpect(status().isNotFound());		
+	}
+	
+	Character choiceRandomCharacter(List<Character> characters) {		
+		Random rand = new Random();		
+		return characters.get(rand.nextInt(characters.size() - 1));
 	}
 
 }
